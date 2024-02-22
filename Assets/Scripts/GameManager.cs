684eb57f3e0855f687eb0c6849ed9d447cf73866
Swimtime1 <aiden.crossfield@gmail.com>
2024-02-22 11:15:23 -0500

    Started GameManager.cs skeleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    // GameObject Variables
+    public GameObject startScreen, winScreen, inGameUI, pauseScreen;
+
+    // TextMeshProUGUI Variables
+
+    // Boolean Variables
+    public static bool gameActive;
+
+    // Integer Variables
+
+    // BoxCollider2D Variables
+
+    // SpriteRenderer Variables
+
+    // Sprite Variables
+
+    // Image Variables
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Deactivates every menu
+    void CloseMenus()
+    {
+        startScreen.SetActive(false);
+        winScreen.SetActive(false);
+        inGameUI.SetActive(false);
+        pauseScreen.SetActive(false);
+    }
+
+    // Opens the Start Screen
+    public void OpenStart()
+    {
+        gameActive = false;
+        CloseMenus();
+        startScreen.SetActive(true);
+    }
+
+    // Opens the Win Screen
+    public void OpenWin()
+    {
+        gameActive = false;
+        CloseMenus();
+        winScreen.SetActive(true);
+    }
+
+    // Opens the In-Game UI
+    public void OpenGameUI()
+    {
+        gameActive = false;
+        CloseMenus();
+        inGameUI.SetActive(true);
+        gameActive = true;
+    }
+
+    // Opens the Pause Menu
+    public void OpenPause()
+    {
+        gameActive = false;
+        CloseMenus();
+        pauseScreen.SetActive(true);
+    }
+}
